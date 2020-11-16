9b127350541c1c4ca4090487731199016650dc43
ch-usamaejaz <ch.usamaejaz@gmail.com>
2020-11-16 13:09:00 +0500

    route added

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/get_user_orders', 'OrdersController@get_user_orders');
+Route::post('/create_new_order', 'OrdersController@create_new_order');
 
 Route::get('/{any}', 'SinglePageController@index')->where('any', '.*');
 
