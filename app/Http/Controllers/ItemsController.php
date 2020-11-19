38c22f2f4a572716e206d1acd3b26792de936cdc
ch-usamaejaz <ch.usamaejaz@gmail.com>
2020-11-19 21:50:26 +0500

    get items route added

---
+++ app/Http/Controllers/ItemsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Items;
+use Illuminate\Http\Request;
+
+class ItemsController extends Controller
+{
+    public function get_all_items()
+    {
+        $response = [];
+        try {
+            $items = Items::select('item_price', 'item_name')->get()->toArray();
+            $response = ['error' => false, 'data' => $items];
+        } catch (\Exception $exception) {
+            $response = ['error' => true, 'message' => $exception->getMessage()];
+        }
+
+        return response()->json($response);
+    }
+}
+
