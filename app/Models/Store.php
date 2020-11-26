8014cd34a04a5a86cbdf8eda87bc7f8aacf3d453
ch-usamaejaz <ch.usamaejaz@gmail.com>
2020-11-26 22:49:04 +0500

    changes

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -3,13 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Store extends Model
 {
     protected $table = 'store';
     protected $fillable = ['item_name', 'item_price', 'quantity'];
     protected $hidden = ['updated_at'];
-    use SoftDeletes;
 
 }
